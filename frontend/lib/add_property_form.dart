1d9deb0e32e794a62a12545fe3c3322b29ac2e58
BabeenDangol <104016877+BabeenDangol@users.noreply.github.com>
2023-07-13 12:32:54 +0545

    change on the addprotertylist

--- frontend/lib/add_property_form.dart
+++ frontend/lib/add_property_form.dart
@@ -1,11 +1,9 @@
-import 'dart:io';
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:intl/intl.dart';
 import 'package:image_picker/image_picker.dart';
 import 'config.dart';
-
 
 class AddPropertyForm extends StatefulWidget {
   @override
@@ -52,45 +50,43 @@ class _AddPropertyFormState extends State<AddPropertyForm> {
   }
 
   void _submitForm() async {
-  final String propertyName = _propertyNameController.text;
-  final String propertyAddress = _propertyAddressController.text;
-  final String propertyRent = _propertyRentController.text;
-  final String propertyType = _selectedPropertyType;
-  final String balconyCount = _selectedBalcony.toString();
-  final String bedroomCount = _selectedBedroom.toString();
-  final String propertyDate = formatter.format(_selectedDate!);
+    final String propertyName = _propertyNameController.text;
+    final String propertyAddress = _propertyAddressController.text;
+    final String propertyRent = _propertyRentController.text;
+    final String propertyType = _selectedPropertyType;
+    final String balconyCount = _selectedBalcony.toString();
+    final String bedroomCount = _selectedBedroom.toString();
+    final String propertyDate = formatter.format(_selectedDate!);
 
-  final Map<String, String> requestBody = {
-    'propertyName': propertyName,
-    'propertyAddress': propertyAddress,
-    'propertyRent': propertyRent,
-    'propertyType': propertyType,
-    'propertyBalconyCount': balconyCount,
-    'propertyBedroomCount': bedroomCount,
-    'propertyDate': propertyDate,
-  };
+    final Map<String, String> requestBody = {
+      'propertyName': propertyName,
+      'propertyAddress': propertyAddress,
+      'propertyRent': propertyRent,
+      'propertyType': propertyType,
+      'propertyBalconyCount': balconyCount,
+      'propertyBedroomCount': bedroomCount,
+      'propertyDate': propertyDate,
+    };
 
-  final encodedData = requestBody.entries
-      .map((e) =>
-          '${Uri.encodeComponent(e.key)}=${Uri.encodeComponent(e.value)}')
-      .join('&');
+    final encodedData = requestBody.entries
+        .map((e) =>
+            '${Uri.encodeComponent(e.key)}=${Uri.encodeComponent(e.value)}')
+        .join('&');
 
-  final response = await http.post(
-    Uri.parse('http://localhost:3000/bookings'),
-    headers: {"Content-Type": "application/json"},
-    body: jsonEncode(requestBody),
-  );
+    final response = await http.post(
+      Uri.parse('http://localhost:3000/bookings'),
+      headers: {"Content-Type": "application/json"},
+      body: jsonEncode(requestBody),
+    );
 
-
-  if (response.statusCode == 200) {
-    // Request successful, do something
-    print('Form submitted successfully');
-  } else {
-    // Request failed, handle the error
-    print('Form submission failed: ${response.reasonPhrase}');
+    if (response.statusCode == 200) {
+      // Request successful, do something
+      print('Form submitted successfully');
+    } else {
+      // Request failed, handle the error
+      print('Form submission failed: ${response.reasonPhrase}');
+    }
   }
-}
-
 
   Container _buildPropertyTypeContainer(
       String type, IconData iconData, bool isSelected) {
@@ -189,7 +185,7 @@ class _AddPropertyFormState extends State<AddPropertyForm> {
                       child: TextField(
                         controller: _propertyNameController,
                         decoration: InputDecoration(
-                          labelText: 'Property Name',
+                          labelText: 'Property address',
                           border: InputBorder.none,
                           contentPadding: EdgeInsets.all(10.0),
                         ),
@@ -209,7 +205,7 @@ class _AddPropertyFormState extends State<AddPropertyForm> {
                       child: TextField(
                         controller: _propertyAddressController,
                         decoration: InputDecoration(
-                          labelText: 'Property Address',
+                          labelText: 'Enter Society/Locality',
                           border: InputBorder.none,
                           contentPadding: EdgeInsets.all(10.0),
                         ),
@@ -236,179 +232,179 @@ class _AddPropertyFormState extends State<AddPropertyForm> {
                       setState(() {
                         _selectedBalcony = number;
                       });
-                    setState(() {
-                      _selectedBalcony = number;
-                    });
-                  },
-                  child: Container(
-                    width: 50.0,
-                    height: 50.0,
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: _selectedBalcony == number
-                            ? Colors.blue
-                            : Colors.grey,
-                        width: 2.0,
-                      ),
-                      borderRadius: BorderRadius.circular(25.0),
-                    ),
-                    child: Center(
-                      child: Text(
-                        number.toString(),
-                        style: TextStyle(
-                          fontSize: 18.0,
-                          fontWeight: _selectedBalcony == number
-                              ? FontWeight.bold
-                              : FontWeight.normal,
+                      setState(() {
+                        _selectedBalcony = number;
+                      });
+                    },
+                    child: Container(
+                      width: 50.0,
+                      height: 50.0,
+                      decoration: BoxDecoration(
+                        border: Border.all(
                           color: _selectedBalcony == number
                               ? Colors.blue
                               : Colors.grey,
+                          width: 2.0,
+                        ),
+                        borderRadius: BorderRadius.circular(25.0),
+                      ),
+                      child: Center(
+                        child: Text(
+                          number.toString(),
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            fontWeight: _selectedBalcony == number
+                                ? FontWeight.bold
+                                : FontWeight.normal,
+                            color: _selectedBalcony == number
+                                ? Colors.blue
+                                : Colors.grey,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                );
-              }),
-            ),
-            const SizedBox(height: 20.0),
-            Text(
-              "Bedrooms",
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 20.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: List.generate(6, (index) {
-                int number = index + 1;
-                return GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      _selectedBedroom = number;
-                    });
-                  },
-                  child: Container(
-                    width: 50.0,
-                    height: 50.0,
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: _selectedBedroom == number
-                            ? Colors.blue
-                            : Colors.grey,
-                        width: 2.0,
-                      ),
-                      borderRadius: BorderRadius.circular(25.0),
-                    ),
-                    child: Center(
-                      child: Text(
-                        number.toString(),
-                        style: TextStyle(
-                          fontSize: 18.0,
-                          fontWeight: _selectedBedroom == number
-                              ? FontWeight.bold
-                              : FontWeight.normal,
+                  );
+                }),
+              ),
+              const SizedBox(height: 20.0),
+              Text(
+                "Bedrooms",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 20.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: List.generate(6, (index) {
+                  int number = index + 1;
+                  return GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        _selectedBedroom = number;
+                      });
+                    },
+                    child: Container(
+                      width: 50.0,
+                      height: 50.0,
+                      decoration: BoxDecoration(
+                        border: Border.all(
                           color: _selectedBedroom == number
                               ? Colors.blue
                               : Colors.grey,
+                          width: 2.0,
+                        ),
+                        borderRadius: BorderRadius.circular(25.0),
+                      ),
+                      child: Center(
+                        child: Text(
+                          number.toString(),
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            fontWeight: _selectedBedroom == number
+                                ? FontWeight.bold
+                                : FontWeight.normal,
+                            color: _selectedBedroom == number
+                                ? Colors.blue
+                                : Colors.grey,
+                          ),
                         ),
                       ),
                     ),
+                  );
+                }),
+              ),
+              const SizedBox(height: 20.0),
+              Text(
+                'Property Rent',
+                style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 10.0),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey,
+                    width: 1.0,
                   ),
-                );
-              }),
-            ),
-            const SizedBox(height: 20.0),
-            Text(
-              'Property Rent',
-              style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 10.0),
-            Container(
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.grey,
-                  width: 1.0,
+                  borderRadius: BorderRadius.circular(5.0),
                 ),
-                borderRadius: BorderRadius.circular(5.0),
-              ),
-              child: TextField(
-                controller: _propertyRentController,
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  labelText: 'Rent Amount',
-                  border: InputBorder.none,
-                  contentPadding: EdgeInsets.all(10.0),
-                ),
-              ),
-            ),
-            const SizedBox(height: 20.0),
-            Text(
-              'Property Rented Date',
-              style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 10.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  _selectedDate == null
-                      ? 'No Date Selected'
-                      : formatter.format(_selectedDate!),
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.bold,
+                child: TextField(
+                  controller: _propertyRentController,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    labelText: 'Rent Amount',
+                    border: InputBorder.none,
+                    contentPadding: EdgeInsets.all(10.0),
                   ),
                 ),
-                IconButton(
-                  onPressed: _presentDatePicker,
-                  icon: const Icon(
-                    Icons.calendar_month,
-                    size: 50,
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 20.0,
-            ),
-            // Text(
-            //   'Property Image',
-            //   style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
-            // ),
-            // const SizedBox(height: 10.0),
-            // GestureDetector(
-            //   onTap: _selectImage,
-            //   child: Container(
-            //     decoration: BoxDecoration(
-            //       border: Border.all(
-            //         color: Colors.grey,
-            //         width: 100.0,
-            //       ),
-            //     ),
-            //     child: _selectedImagePath.isNotEmpty
-            //         ? Image.network(_selectedImagePath)
-            //         : Container(
-            //             height: 100.0,
-            //             child: Icon(
-            //               Icons.image,
-            //               size: 50.0,
-            //               color: Colors.grey,
-            //             ),
-            //           ),
-            //   ),
-            // ),
-            const SizedBox(height: 20.0),
-            ElevatedButton(
-              style: ButtonStyle(
-                elevation: MaterialStateProperty.all(12),
               ),
-              onPressed: _submitForm,
-              child: Text('Submit'),
-            ),
-          ],
+              const SizedBox(height: 20.0),
+              Text(
+                'Property Rented Date',
+                style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 10.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    _selectedDate == null
+                        ? 'No Date Selected'
+                        : formatter.format(_selectedDate!),
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: _presentDatePicker,
+                    icon: const Icon(
+                      Icons.calendar_month,
+                      size: 50,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              // Text(
+              //   'Property Image',
+              //   style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+              // ),
+              // const SizedBox(height: 10.0),
+              // GestureDetector(
+              //   onTap: _selectImage,
+              //   child: Container(
+              //     decoration: BoxDecoration(
+              //       border: Border.all(
+              //         color: Colors.grey,
+              //         width: 100.0,
+              //       ),
+              //     ),
+              //     child: _selectedImagePath.isNotEmpty
+              //         ? Image.network(_selectedImagePath)
+              //         : Container(
+              //             height: 100.0,
+              //             child: Icon(
+              //               Icons.image,
+              //               size: 50.0,
+              //               color: Colors.grey,
+              //             ),
+              //           ),
+              //   ),
+              // ),
+              const SizedBox(height: 20.0),
+              ElevatedButton(
+                style: ButtonStyle(
+                  elevation: MaterialStateProperty.all(12),
+                ),
+                onPressed: _submitForm,
+                child: Text('Submit'),
+              ),
+            ],
+          ),
         ),
       ),
-    ),
     );
   }
 }
--- frontend/lib/config.dart
+++ frontend/lib/config.dart
@@ -3,8 +3,7 @@
 // final login = url + 'login';
 // final createProperty = url + 'createProperty';
 
-
-final url = 'http://localhost:3000/';
+final url = 'http://192.168.90.90:3000/';
 final registration = url + "registration";
 final login = url + 'login';
 final createBooking = 'bookings';
